0bb125813553a7aaed99ac808f996f61da66725e
diaokaizi <1433137915@qq.com>
2024-10-03 17:52:04 +0800

    123

---
+++ f-anogan/tools.py
@@ -0,0 +1,58 @@
+import torch
+from torchvision import datasets
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import Lasso
+from sklearn.preprocessing import StandardScaler
+
+class NormalizeTransform:
+    """ Normalize features with mean and standard deviation. """
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, sample):
+        return (sample - self.mean) / self.std
+
+class SimpleDataset(torch.utils.data.Dataset):
+    def __init__(self, data, labels, transform=None):
+        self.transform = transform
+        self.data = data
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        data = self.data[idx]
+        labels = self.labels[idx]
+
+        if self.transform:
+            data = self.transform(data)
+
+        return data, labels
+
+    def __len__(self):
+        return len(self.data)
+
+def fix_name():
+    return ["sportsocks", "sportprivate", "dportirc", "sporttelnet", "sportrapservice", "dporthttp",
+            "sportsyslog", "sportreserved", "dportkpasswd", "tcpflagsACK", "npacketsmedium",
+            "sportcups", "dporttelnet", "sportldaps", "tcpflagsPSH", "dportoracle"]
+
+def load_UGR16():
+    raw_x_train = pd.read_csv("/root/bishe/dataset/URD16/UGR16v1.Xtrain.csv").drop(columns=["Row"], axis=1)
+    x_train = raw_x_train
+    x_train = torch.from_numpy(x_train.values).float()
+    y_train = torch.zeros(len(x_train))
+
+
+    raw_x_test = pd.read_csv("/root/bishe/dataset/URD16/UGR16v1.Xtest.csv").drop(columns=["Row"], axis=1)
+    x_test = raw_x_test
+    x_test = torch.from_numpy(x_test.values).float()
+    y_test = pd.read_csv("/root/bishe/dataset/URD16/UGR16v1.Ytest.csv").drop(columns=["Row", "labelanomalyidpscan", "labelanomalysshscan", "labelanomalyidpscan", "labelblacklist"], axis=1)
+    y_test = torch.from_numpy(y_test.apply(lambda row: 1 if row.sum() > 0 else 0, axis=1).values)
+    return (x_train, y_train), (x_test, y_test)
+
+def load_UGR16_gas():
+    raw_x_train = pd.read_csv("/root/GSA-AnoGAN/KitNet/gsa.csv", header=None).drop(columns=[0], axis=1)
+    x_train = torch.from_numpy(raw_x_train.values).float()
+    y_train = torch.zeros(len(x_train))
+    return (x_train, y_train), (0, 0)
